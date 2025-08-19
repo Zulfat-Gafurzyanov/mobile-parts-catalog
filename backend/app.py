a091fa1eb35909288bc5544b1ddc6f9b236f80a5
Zulfat-Gafurzyanov <gafurzyanov.zulfat@gmail.com>
2025-08-19 10:29:53 +1000

    добавил cors

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,26 @@
-from flask import Flask, jsonify
-import pandas as pd
 import os
 import logging
 
+from environs import Env
+from flask import Flask, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+
+env = Env()
+env.read_env()
 app = Flask(__name__)
+
+server_ip = env.str("SERVER_IP")
+# Настройка CORS
+CORS(app,
+     origins=[
+         "https://web.telegram.org",  # Telegram Web Apps
+         "https://your-domain.com",   # Ваш домен
+         f"http://{server_ip}/api/get_data",
+     ],
+     allow_headers=["Content-Type", "Authorization"],
+     methods=("GET",))
 
 # Настройка логирования
 logging.basicConfig(
@@ -13,21 +30,35 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Путь к файлу
-EXCEL_FILE_PATH = os.path.join('input_data', 'Остатки (4).xlsx')
+EXCEL_FILE_PATH = os.path.join('..', 'input_data', 'Остатки (4).xlsx')
 
+
+# Маршрут для проверки CORS
+@app.route('/api/test-cors', methods=['GET'])
+def test_cors():
+    """Тестовый эндпоинт для проверки CORS"""
+    return jsonify({
+        "message": "CORS is working!",
+        "allowed_origins": "*",  # или список разрешенных доменов
+        "timestamp": pd.Timestamp.now().isoformat()
+    }), 200
+
+
+# Маршрут для проверки состояния сервиса
 @app.route('/health', methods=['GET'])
 def health_check():
-    """Проверка состояния сервиса"""
     return jsonify({"status": "healthy"}), 200
 
+
+# Проверка наличия файла и чтение данных из XLSX
 @app.route('/api/get_data', methods=['GET'])
 def get_data():
     try:
         # Проверка существования файла
         if not os.path.exists(EXCEL_FILE_PATH):
-            logger.error(f"File not found: {EXCEL_FILE_PATH}")
+            logger.error(f"Файл по пути: {EXCEL_FILE_PATH} не найден")
             return jsonify({"error": "Data file not found"}), 404
-        
+
         # Чтение данных из XLSX-файла
         df = pd.read_excel(EXCEL_FILE_PATH, engine='openpyxl')
         logger.info(f"Successfully loaded {len(df)} rows from Excel file")
@@ -63,7 +94,7 @@ def get_data():
                     technics_dict[model].append(model_dict)
                 else:
                     technics_dict[model] = [model_dict]
-                    
+
             except Exception as e:
                 logger.warning(f"Error processing row {idx}: {str(e)}")
                 continue
@@ -74,6 +105,7 @@ def get_data():
     except Exception as e:
         logger.error(f"Error processing request: {str(e)}")
         return jsonify({"error": "Internal server error"}), 500
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
